0c4bbcb0d035ea3ad803e7baf565268e422ac0f1
Kazuhiro Miyahara <kazuhiro.miyahara.vs@gmail.com>
2021-07-27 04:01:12 +0900

    broken

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,10 @@ class _MyHomePageState extends State<MyHomePage> {
             videoInputs.add(element as MediaDeviceInfo);
           }
         });
+      }).whenComplete(() {
+        setState(() {
+          selectedDevice = videoInputs.first;
+        });
       });
     }
   }
