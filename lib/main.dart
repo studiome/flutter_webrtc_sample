dc3597321bb43eb5312e54f2e8cda787b0892306
Kazuhiro Miyahara <kazuhiro.miyahara.vs@gmail.com>
2021-08-15 00:42:26 +0900

    dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      darkTheme: ThemeData.dark(),
       home: MyHomePage(title: 'WebRTC Sample for Flutter Web'),
     );
   }
